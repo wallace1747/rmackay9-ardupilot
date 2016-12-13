165e09ba63760135bae1450073538da616ca62e6
Randy Mackay <rmackay9@yahoo.com>
2016-12-13 16:13:04 +0900

    AP_MotorsSingle: add set_frame_class_and_type

--- libraries/AP_Motors/AP_MotorsSingle.cpp
+++ libraries/AP_Motors/AP_MotorsSingle.cpp
@@ -81,6 +81,11 @@ void AP_MotorsSingle::Init()
 
     // allow mapping of motor7
     add_motor_num(CH_7);
+
+// set frame class (i.e. quad, hexa, heli) and type (i.e. x, plus)
+void AP_MotorsSingle::set_frame_class_and_type(motor_frame_class frame_class, motor_frame_type frame_type)
+{
+    _flags.initialised_ok = (frame_class == MOTOR_FRAME_SINGLE);
 }
 
 // set update rate to motors - a value in hertz
